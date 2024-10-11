c640a620168397d4ecbd21aecd3cd3092fc99888
Leonardo Davalos <32350741+leothecrz@users.noreply.github.com>
2024-10-10 18:14:34 -0700

    camera rig

--- Scripts/car_base.gd
+++ Scripts/car_base.gd
@@ -1,10 +1,11 @@
 extends Node3D
 
-@onready var FL:RayCast3D = $RigidBody3D/FrontLeft;
-@onready var FR:RayCast3D = $RigidBody3D/FrontRight;
-@onready var BL:RayCast3D = $RigidBody3D/BackLeft;
-@onready var BR:RayCast3D = $RigidBody3D/BackRight;
+@onready var FL:RayCast3D = $Casters/FrontLeft;
+@onready var FR:RayCast3D = $Casters/FrontRight;
+@onready var BL:RayCast3D = $Casters/BackLeft;
+@onready var BR:RayCast3D = $Casters/BackRight;
 
+@onready var CAM = $CamController;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,7 +15,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
-	if(FL.is_colliding()):
-		print("is colliding")
+	
+	
+	
+	CAM.position = lerp(CAM.position, position, 0.01);
 	
 	pass
